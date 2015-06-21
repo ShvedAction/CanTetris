0cce7c20e2e27204f5e79fb7432495f607638f26
ShvedActio <druid_aa@mail.ru>
2015-06-21 23:51:36 +0300

    first commit

---
+++ CanTetris.js
@@ -0,0 +1,11 @@
+describe("CanTetris", function() {
+
+  beforeEach(function() {
+    console.log("in beforeEach");
+  });
+
+  it("should be", function() {
+	expect(true).toEqual(true);
+  });
+
+});
