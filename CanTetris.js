5cfb8bdf26f25bf8281e9f4f4d7755f13cd88d00
ShvedActio <druid_aa@mail.ru>
2015-06-26 23:12:33 +0300

    reflectOnField

--- CanTetris.js
+++ CanTetris.js
@@ -1,29 +1,43 @@
 var Figure, field;
 var fieldHeight = 30, fieldWidth = 10;
 $(document).ready(function(){
-  var ALL_TYPE_FIGURE = [
-      [{x:-2 ,y:0},{x:-1 ,y:0},{x:0 ,y:0},{x:1 ,y:0}]   //палка
+    var ALL_TYPE_FIGURE = [
+        [{x:-2 ,y:0},{x:-1 ,y:0},{x:0 ,y:0},{x:1 ,y:0}]   //палка
     ];
     
-  Figure = can.Model.extend({},{
-    posx: 5,
-    posy: 30,
-    init: function(){
-      var randIndex = Math.floor(Math.random() * (ALL_TYPE_FIGURE.length - 1));
-      this.attr('cells', ALL_TYPE_FIGURE[randIndex]);
-    },
-    come_down: function(){
-      this.posy--;
-    },
-    rotate: function(){ return 0;}
-  });
-  
-  //create field
-  field = new can.List([]);
-  for (var i = 0; i < (fieldHeight*fieldWidth); i++){
-    field.attr(i, new can.Model({state: "empty"}));
-  }
-  field.getXY = function(x,y){
-    return this.attr(y*fieldWidth + x);
-  }
+    Figure = can.Model.extend({},{
+        posx: 5,
+        posy: 29,
+        init: function(){
+            var randIndex = Math.floor(Math.random() * (ALL_TYPE_FIGURE.length - 1));
+            this.attr('cells', ALL_TYPE_FIGURE[randIndex]);
+            this.reflectOnField();
+        },
+        come_down: function(){
+            this.attr("posy", this.posy-1);
+        },
+        getPosCells: function(){
+            var result = [], _this = this;
+            this.cells.each(function(cell){
+                result.push({x: cell.x + _this.posx, y: cell.y + _this.posy});
+            });
+            return result;
+        },
+        reflectOnField: function (){
+            var _this = this;
+            can.each(_this.getPosCells(), function(val){
+                field.getXY(val.x, val.y).attr("state", "red");
+            });
+        },
+        rotate: function(){ return 0;}
+    });
+    
+    //create field
+    field = new can.List([]);
+    for (var i = 0; i < (fieldHeight*fieldWidth); i++){
+        field.attr(i, new can.Model({state: "empty"}));
+    }
+    field.getXY = function(x,y){
+        return this.attr(y*fieldWidth + x);
+    }
 });
--- CanTetrisSpec.js
+++ CanTetrisSpec.js
@@ -1,40 +1,56 @@
 describe("figure", function() {
-  var targetFigure;
-  beforeEach(function() {
-    targetFigure = new Figure();
-  });
-
-  afterEach(function(){
+    var targetFigure;
+    beforeEach(function() {
+        targetFigure = new Figure();
+    });
     
-  });
-  
-  it("should be create on center and top", function() {
-    expect(targetFigure.posx).toEqual(5);
-    expect(targetFigure.posy).toEqual(30);
-  });
-
-  it("in the come-down should be reduce posy", function(){
-    targetFigure.come_down();
-    expect(targetFigure.posy).toEqual(29);
-  });
-  
-  it("on create should be random", function (){
-    //todo may be
-  });
-  
-  it("should be able to rotate", function(){
-    expect(targetFigure.rotate).not.toThrow();
-  });
+    afterEach(function(){
+    
+    });
+    
+    it("should be create on center and top", function() {
+        expect(targetFigure.posx).toEqual(5);
+        expect(targetFigure.posy).toEqual(29);
+    });
+    
+    it("in the come-down should be reduce posy", function(){
+        targetFigure.come_down();
+        expect(targetFigure.posy).toEqual(28);
+    });
+    
+    it("on create should be random", function (){
+        //todo may be
+    });
+    
+    it("should be able to rotate", function(){
+        expect(targetFigure.rotate).not.toThrow();
+    });
+    
+    it("should be able to return the occupied cells, by method: getPosCells", function(){
+        var i = 0;
+        can.each(targetFigure.getPosCells(),function(posCell, index){
+            expect(posCell.x).toEqual(targetFigure.cells[index].x + targetFigure.posx);
+            expect(posCell.y).toEqual(targetFigure.cells[index].y + targetFigure.posy);
+        });
+    });
+    
+    describe("should be reflect on field", function(){
+        it("when creating the figure cells underneath the field must be filled", function(){
+            targetFigure = new Figure();
+            can.each(targetFigure.getPosCells(), function(val, index){
+                expect(field.getXY(val.x, val.y).state).not.toEqual("empty");
+            });
+        });
+    });
 });
 
 describe("field", function(){
-  it("should be the double array 5x30 collapsed to the list: length == 300", function(){
-    expect(field.attr("length")).toEqual(300);
-
-  });
-  
-  it("should be able to getXY",function(){
-    expect(function(){return field.getXY(3,12);}).not.toThrow();
-    expect(field.attr(32)).toBe(field.getXY(2,3));
-  });
+    it("should be the double array 5x30 collapsed to the list: length == 300", function(){
+        expect(field.attr("length")).toEqual(300);
+    });
+    
+    it("should be able to getXY",function(){
+        expect(function(){return field.getXY(3,12);}).not.toThrow();
+        expect(field.attr(32)).toBe(field.getXY(2,3));
+    });
  });
